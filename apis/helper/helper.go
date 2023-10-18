722ebfd6ff88889cc37135685706b40170c12e8d
aniket-mdev <aniketsuryawanshidev@gmail.com>
2023-10-18 12:19:23 +0530

    remove print statement from helper class

--- apis/helper/helper.go
+++ apis/helper/helper.go
@@ -1,13 +1,10 @@
 package helper
 
 import (
-	"fmt"
-
 	"github.com/aniket-skroman/skroman_support_installation/utils"
 )
 
 func SetPaginationData(page int, total int64) {
-	fmt.Println("Data from set : ", page, total)
 	if page == 0 {
 		utils.PREVIOUS_IDX = 0
 	} else {
