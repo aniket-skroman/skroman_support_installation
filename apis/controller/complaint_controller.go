3f8211b2ee482bb14acc7b7676d55b60575162f3
aniket-mdev <aniketsuryawanshidev@gmail.com>
2023-12-12 15:22:15 +0530

    add a content types for image and video files

--- apis/controller/complaint_controller.go
+++ apis/controller/complaint_controller.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"path"
+	"strings"
 
 	"github.com/aniket-skroman/skroman_support_installation/apis/dto"
 	"github.com/aniket-skroman/skroman_support_installation/apis/helper"
@@ -150,7 +151,14 @@ func (cont *complaint_controller) FetchDeviceImageURL(ctx *gin.Context) {
 
 	respo := generateSignedS3URL(req.FilePath, req.Directory)
 	defer respo.Body.Close()
-	ctx.DataFromReader(http.StatusOK, *respo.ContentLength, *respo.ContentType, respo.Body, nil)
+	var content string
+	if strings.Contains(req.FilePath, ".MP4") || strings.Contains(req.FilePath, ".mp4") {
+		content = "video/mp4"
+	} else {
+		content = *respo.ContentType
+	}
+
+	ctx.DataFromReader(http.StatusOK, *respo.ContentLength, content, respo.Body, nil)
 }
 
 func (cont *complaint_controller) FetchPDFFile(ctx *gin.Context) {
